06b85b033b23b5c49946379b0cc5b2f7138a586d
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2020-07-09 15:54:44 +0200

    Correct name of logging app env var

--- db-administration-app/src/util/logger.js
+++ db-administration-app/src/util/logger.js
@@ -13,7 +13,7 @@ class LoggingServiceTransport extends Transport {
 
     postToLogService(data, logResourcePath) {
         axios
-            .post(`http://${process.env.LOG_APP_URL}/log/${logResourcePath}`, {
+            .post(`http://${process.env.LOGGING_APP_URL}/log/${logResourcePath}`, {
                 appName: "db-access",
                 timestamp: new Date().toISOString(),
                 data: data
